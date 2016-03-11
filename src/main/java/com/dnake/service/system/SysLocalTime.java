8392e4ec1ad11f5e50e01face078db3e831b8270
13346450 <13346450@qq.com>
2016-03-11 16:13:09 +0800

    aaaaa

--- src/main/java/com/dnake/service/system/SysLocalTime.java
+++
@@ -1,8 +0,0 @@
-package com.dnake.service.system;
-
-import com.dnake.domain.common.MobileParms;
-
-public interface SysLocalTime {
-//获取系统时间
-String getLocaltime(MobileParms parms);
-}
--- src/main/java/com/dnake/tasks/lhApi/LhListItemHandler.java
+++
@@ -1,5 +0,0 @@
-package com.dnake.tasks.lhApi;
-
-public interface LhListItemHandler<T> {
-	public void execute(LhListResult<T> root,T item);
-}
